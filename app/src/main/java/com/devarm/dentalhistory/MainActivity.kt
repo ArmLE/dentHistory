3a8261776860e83767d2394c528082c26c958440
ArmandoLopez <alopeze@unsa.edu.pe>
2022-07-27 11:59:59 -0500

    first commit

---
+++ app/src/main/java/com/devarm/dentalhistory/MainActivity.kt
@@ -0,0 +1,111 @@
+package com.devarm.dentalhistory
+
+//import android.hardware.biometrics.BiometricManager
+import android.content.Context
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.biometric.BiometricManager
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
+import androidx.biometric.BiometricPrompt
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import com.devarm.dentalhistory.ui.theme.DentalHistoryTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DentalHistoryTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    //Greeting("Android")
+                    Auth()
+                }
+            }
+        }
+
+        setupAuth()
+    }
+
+    private var canAuth = false
+    private lateinit var promptInfo:BiometricPrompt.PromptInfo
+
+    private fun setupAuth(){
+        if(BiometricManager.from(this).canAuthenticate(BIOMETRIC_STRONG)==BiometricManager.BIOMETRIC_SUCCESS)
+            canAuth=true
+            promptInfo=BiometricPrompt.PromptInfo.Builder().setTitle("Authenticacion Biometrica").setAllowedAuthenticators(
+                BIOMETRIC_STRONG).build()
+    }
+    private fun authenticate(auth:(auth:Boolean)->Unit){
+        if(canAuth){
+            BiometricPrompt(this, ContextCompat.getMainExecutor(this),
+            object: BiometricPrompt.AuthenticationCallback(){
+                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                    super.onAuthenticationSucceeded(result)
+
+                    auth(true)
+                }
+            }).authenticate(promptInfo)
+        }else{
+            auth
+        }
+    }
+
+    @Composable
+    fun Auth(){
+
+        var auth by remember {
+            mutableStateOf(false)
+        }
+
+        Column(modifier = Modifier
+            .background( if(auth)  Color(0xFF93E2E2)  else Color(0xFFFFFFFF) )
+            .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center) {
+            Text(text = if(auth) "Estas Auth" else "Necesitas Auth" ,fontWeight = FontWeight.Bold)
+            Spacer(modifier = Modifier.height(8.dp))
+            Button(onClick = {
+                    if(auth) {
+                        auth=false
+                    }else{
+                        authenticate {auth=it}
+                    }
+                }) {
+                Text(text = if(auth) "Cerrar Session" else "Authenticar")
+            }
+        }
+    }
+/*
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}*/
+
+    @Preview(showSystemUi = true)
+    @Composable
+    fun DefaultPreview() {
+        DentalHistoryTheme {
+            //Greeting("Android")
+            Auth()
+        }
+    }
+}
+
+
